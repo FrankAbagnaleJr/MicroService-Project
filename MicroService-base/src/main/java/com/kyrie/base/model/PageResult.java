0207cba8526ce5dd892689225996aa5048ba9cf0
jijinliang <625103471@qq.com>
2023-07-02 18:09:21 +0800

    base模块定义分页返回对象模型

---
+++ MicroService-base/src/main/java/com/kyrie/base/model/PageResult.java
@@ -0,0 +1,31 @@
+package com.kyrie.base.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @auther: jijin
+ * @date: 2023/7/2 17:58 周日
+ * @project_name: MicroService-Project
+ * @version: 1.0
+ * @description TODO
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResult<T> implements Serializable {
+
+    //返回的数据列表
+    private List<T> items;
+    //总记录输
+    private Long counts;
+    //当前页码
+    private Long page;
+    //每页记录数
+    private Long pageSize;
+
+}
