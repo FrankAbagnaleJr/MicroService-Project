64df9e82677dd27b4030e8c4fad67c088b44b81b
jijinliang <625103471@qq.com>
2023-07-02 17:41:32 +0800

    user添加swagger接口文档配置类，修改默认的标题作者等

---
+++ MicroService-user/src/main/java/com/kyrie/user/config/SwaggerDocumentationConfig.java
@@ -0,0 +1,44 @@
+package com.kyrie.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @auther: jijin
+ * @date: 2023/7/2 17:17 周日
+ * @project_name: MicroService-Project
+ * @version: 1.0
+ * @description TODO
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerDocumentationConfig {
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("用户管理接口")
+                .description("用户管理接口 description")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .termsOfServiceUrl("")
+                .version("1.0.0")
+                .contact(new Contact("冀金梁", "http://wimoor.com", "625103471@qq.com"))
+                .build();
+    }
+
+    @Bean
+    public Docket customImplementation() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.kyrie.user"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+}
--- MicroService-user/src/main/java/com/kyrie/user/controller/UserContraller.java
+++ MicroService-user/src/main/java/com/kyrie/user/controller/UserContraller.java
@@ -2,6 +2,8 @@ package com.kyrie.user.controller;
 
 import com.kyrie.user.pojo.User;
 import com.kyrie.user.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,11 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/user")
+@Api(value = "用户管理接口",tags = "用户管理接口tags")
 public class UserContraller {
 
     @Autowired
     UserService userService;
 
+    @ApiOperation("根据用户id查用户")
     @GetMapping("/{id}")
     public User getById(@PathVariable("id") Long id) {
         return userService.getUserById(id);
