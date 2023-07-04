0c7ace1d826f392a75d9358a29c98bb80e749f17
冀金梁 <625103471@qq.com>
2023-07-04 10:53:14 +0800

    成功打开swagger-ui界面：但是必须@RequestMapping 跟 nacos中context-path路径 同时配置/user才能打开，但是数据访问得加两层/user/user

--- MicroService-order/src/main/java/com/kyrie/order/config/SwaggerDocumentationConfig.java
+++ MicroService-order/src/main/java/com/kyrie/order/config/SwaggerDocumentationConfig.java
@@ -36,7 +36,7 @@ public class SwaggerDocumentationConfig {
     @Bean
     public Docket customImplementation() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .enable(true)
+//                .enable(true)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.kyrie.order.controller"))
                 .build()
