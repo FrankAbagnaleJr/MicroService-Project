4289a3855cb37cc43f97ac5facc11fe75b2eae45
jijinliang <625103471@qq.com>
2023-07-04 23:52:05 +0800

    自定义异常：完成，写了拦截自定义异常跟全局异常的 异常拦截器

--- MicroService-base/src/main/java/com/kyrie/base/exception/CommonError.java
+++ MicroService-base/src/main/java/com/kyrie/base/exception/CommonError.java
@@ -1,4 +1,17 @@
 package com.kyrie.base.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum CommonError {
+
+    UNKOWN_ERROR("执行过程异常，请重试。"),
+    PARAMS_ERROR("非法参数"),
+    OBJECT_NULL("对象为空"),
+    QUERY_NULL("查询结果为空"),
+    REQUEST_NULL("请求参数为空");
+
+    private String errMessage;
 }
--- MicroService-base/src/main/java/com/kyrie/base/exception/MicServiceException.java
+++ MicroService-base/src/main/java/com/kyrie/base/exception/MicServiceException.java
@@ -1,5 +1,8 @@
 package com.kyrie.base.exception;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @auther: jijin
  * @date: 2023/7/3 23:55 周一
@@ -7,7 +10,19 @@ package com.kyrie.base.exception;
  * @version: 1.0
  * @description 本项目自定义异常类型，继承RuntimeException
  */
+@NoArgsConstructor
+@Data
 public class MicServiceException extends RuntimeException{
     private String errMessage;
 
+    public MicServiceException(String errMessage) {
+        super(errMessage);
+        this.errMessage = errMessage;
+    }
+    public static void cast(String message){
+        throw new MicServiceException(message);
+    }
+    public static void cast(CommonError error){
+        throw new MicServiceException(error.getErrMessage());
+    }
 }
