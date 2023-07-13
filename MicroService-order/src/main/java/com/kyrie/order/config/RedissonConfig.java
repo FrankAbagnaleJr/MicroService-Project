cddea86658b0b0cf119ea136cbab318afbb82062
jijinliang <625103471@qq.com>
2023-07-13 22:34:15 +0800

    order模块：添加redis跟redisson配置类

---
+++ MicroService-order/src/main/java/com/kyrie/order/config/RedissonConfig.java
@@ -0,0 +1,23 @@
+package com.kyrie.order.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+    @Bean
+    public RedissonClient redissonClient(){
+        //配置类
+        Config config = new Config();
+
+        //配置地址端口
+        config.useClusterServers().addNodeAddress("redis://192.168.101.65:6379").setPassword("redis");
+
+        //把配置类给客户端，然后返回客户端
+        return Redisson.create(config);
+    }
+}
