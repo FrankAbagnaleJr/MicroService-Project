035d2843ba30d54f50abb946360e458d9fe31d8f
jijinliang <625103471@qq.com>
2023-07-13 23:34:53 +0800

    order模块：redis缓存完成（未测试）

--- MicroService-order/src/main/java/com/kyrie/order/sercice/impl/IOrderServiceImpl.java
+++ MicroService-order/src/main/java/com/kyrie/order/sercice/impl/IOrderServiceImpl.java
@@ -10,6 +10,8 @@ import com.kyrie.order.dto.QueryOrderParamsDto;
 import com.kyrie.order.mapper.OrderMapper;
 import com.kyrie.order.pojo.Order;
 import com.kyrie.order.sercice.IOrderService;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -28,31 +30,58 @@ public class IOrderServiceImpl extends ServiceImpl<OrderMapper, Order> implement
     @Autowired
     RedisTemplate redisTemplate;
 
+    @Autowired
+    RedissonClient redissonClient;
+
     @Override
     public IPage<Order> queryOrderList(PageParams pageParams, QueryOrderParamsDto queryOrderParamsDto) {
         Page<Order> ipage = new Page<>(pageParams.getPageNum(), pageParams.getPageSize());
         LambdaQueryWrapper lqw = new LambdaQueryWrapper<>();
         Page<Order> page = orderMapper.selectPage(ipage, lqw);
-        page.getRecords().stream().forEach(o -> {o.setUser(orderClient.selectById(o.getUserId()));});
+        page.getRecords().stream().forEach(o -> {
+            o.setUser(orderClient.selectById(o.getUserId()));
+        });
         return page;
     }
 
     @Override
     public Order queryById(Long id) {
 
-//        Order order = null;
-//
-//        //先从缓存中查，查到直接返回
-//        order = (Order) redisTemplate.opsForValue().get("order:" + id);
-//        if (order!=null) return order;
-//
-//        //缓存没有，查数据库
-//        order = orderMapper.selectById(id);
-//        if (order == null) {
-//            //都没有就缓存个空值防止缓存穿透
-//            redisTemplate.opsForValue().set("order:"+id,null,new Random().nextInt(10)+1, TimeUnit.SECONDS);
-//        }
+        Order order = null;
 
-        return null;
+        //先从缓存中查，查到直接返回
+        order = (Order) redisTemplate.opsForValue().get("order:" + id);
+        if (order != null) return order;
+
+        //缓存没有，查数据库，查数据库得先加锁，不然访问都打到数据库，会redis击穿
+        //先得到redisson锁
+        RLock orderlock = redissonClient.getLock("orderlock");
+        try {
+            //尝试加锁
+            boolean islock = orderlock.tryLock(1, 10, TimeUnit.SECONDS);
+            //没得到锁，自旋重试
+            if (!islock) {
+                Thread.sleep(100);
+                return queryById(id);
+            }
+
+            //得到锁，查询数据库
+            order = orderMapper.selectById(id);
+            //都没有就缓存个空值防止缓存穿透
+            if (order == null) {
+                redisTemplate.opsForValue().set("order:" + id, null, new Random().nextInt(10) + 1, TimeUnit.SECONDS);
+                return null;
+            }
+
+            //数据库查到了，回写redis并返回
+            redisTemplate.opsForValue().set("order:"+id,order);
+            return order;
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            //解锁
+            orderlock.unlock();
+        }
     }
 }
