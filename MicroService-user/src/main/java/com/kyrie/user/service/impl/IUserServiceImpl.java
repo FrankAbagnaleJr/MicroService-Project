6905871673137275a2d3b349b6167ac5227856a0
冀金梁 <625103471@qq.com>
2023-07-13 08:47:57 +0800

    user模块：防止缓存穿透 - 并发查数据库时先加锁，只有一个线程能拿到锁去查mysql，防止缓存击穿

--- MicroService-user/src/main/java/com/kyrie/user/service/impl/IUserServiceImpl.java
+++ MicroService-user/src/main/java/com/kyrie/user/service/impl/IUserServiceImpl.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.concurrent.TimeUnit;
 
 @Service
-public class IUserServiceImpl extends ServiceImpl<UserMapper,User> implements IUserService {
+public class IUserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
     @Autowired
     UserMapper userMapper;
@@ -28,7 +28,7 @@ public class IUserServiceImpl extends ServiceImpl<UserMapper,User> implements IU
     private final String RED_USER = "user:";
 
     @Override
-    public IPage queryUserList(PageParams pageParams,@RequestBody(required = false) QueryUserParamsDto queryUserParamsDto) {
+    public IPage queryUserList(PageParams pageParams, @RequestBody(required = false) QueryUserParamsDto queryUserParamsDto) {
         Page page = new Page(pageParams.getPageNum(), pageParams.getPageSize());
         LambdaQueryWrapper lqw = new LambdaQueryWrapper();
         return userMapper.selectPage(page, lqw);
@@ -40,23 +40,26 @@ public class IUserServiceImpl extends ServiceImpl<UserMapper,User> implements IU
 
         //先从redis中查，查到了直接返回
         user = (User) redisTemplate.opsForValue().get(RED_USER + id);
-        if (user!=null) return user;
+        if (user != null) return user;
 
-        //redis中没有从数据库查，查到了回写redis，并返回
-        user = userMapper.selectById(id);
-        if (user!=null) {
-//            synchronized (this) {
-//
-//                user = (User) redisTemplate.opsForValue().get(RED_USER + id);
-//                if (user != null) return user;
+        // TODO 缓存击穿 - redis中没有从数据库查，查数据库时先加锁，防止缓存击穿
+        synchronized (this) {
+            user = userMapper.selectById(id);
 
-                redisTemplate.opsForValue().set(RED_USER+id,user,10, TimeUnit.SECONDS);
-//            }
-            return user;
+            //数据库查到了，再查redis，防止拿锁时已经有线程回写redis了
+            if (user != null) {
+
+                User user2 = (User) redisTemplate.opsForValue().get(RED_USER + id);
+                //这时候说明加锁前已经被别的线程缓存redis了，不需要再回写，直接返回redis中的数据
+                if (user2 != null) return user2;
+
+                //这时候说明加锁后redis确定没有数据，可以回写
+                redisTemplate.opsForValue().set(RED_USER + id, user, 10, TimeUnit.SECONDS);
+            }
         }
 
-        //redis跟数据库都没，回写redis个空值
-        redisTemplate.opsForValue().set(RED_USER,null,10,TimeUnit.SECONDS);
+        // TODO 缓存穿透 - redis跟数据库都没，回写redis个空值，防止缓存穿透
+        redisTemplate.opsForValue().set(RED_USER, null, 10, TimeUnit.SECONDS);
         return user;
     }
 }
