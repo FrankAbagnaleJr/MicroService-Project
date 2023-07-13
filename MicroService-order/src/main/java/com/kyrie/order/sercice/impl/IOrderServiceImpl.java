553eabf3b453e6f168240b307f4ea61d129eec2a
jijinliang <625103471@qq.com>
2023-07-13 22:45:36 +0800

    order模块：添加查询单个用户接口，用来做redis缓存

--- MicroService-order/src/main/java/com/kyrie/order/sercice/impl/IOrderServiceImpl.java
+++ MicroService-order/src/main/java/com/kyrie/order/sercice/impl/IOrderServiceImpl.java
@@ -11,7 +11,11 @@ import com.kyrie.order.mapper.OrderMapper;
 import com.kyrie.order.pojo.Order;
 import com.kyrie.order.sercice.IOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class IOrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements IOrderService {
@@ -21,6 +25,9 @@ public class IOrderServiceImpl extends ServiceImpl<OrderMapper, Order> implement
     @Autowired
     OrderClient orderClient;
 
+    @Autowired
+    RedisTemplate redisTemplate;
+
     @Override
     public IPage<Order> queryOrderList(PageParams pageParams, QueryOrderParamsDto queryOrderParamsDto) {
         Page<Order> ipage = new Page<>(pageParams.getPageNum(), pageParams.getPageSize());
@@ -28,5 +35,24 @@ public class IOrderServiceImpl extends ServiceImpl<OrderMapper, Order> implement
         Page<Order> page = orderMapper.selectPage(ipage, lqw);
         page.getRecords().stream().forEach(o -> {o.setUser(orderClient.selectById(o.getUserId()));});
         return page;
+    }
+
+    @Override
+    public Order queryById(Long id) {
+
+//        Order order = null;
+//
+//        //先从缓存中查，查到直接返回
+//        order = (Order) redisTemplate.opsForValue().get("order:" + id);
+//        if (order!=null) return order;
+//
+//        //缓存没有，查数据库
+//        order = orderMapper.selectById(id);
+//        if (order == null) {
+//            //都没有就缓存个空值防止缓存穿透
+//            redisTemplate.opsForValue().set("order:"+id,null,new Random().nextInt(10)+1, TimeUnit.SECONDS);
+//        }
+
+        return null;
     }
 }
